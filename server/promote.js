eda3328c70c77033a731b6ec9d36c1cfab2dd7e3
Janvi Sharma <sjjanvi2121@gmail.com>
2026-03-12 16:42:49 +0530

    fix start script

--- server/promote.js
+++ server/promote.js
@@ -12,7 +12,7 @@ const promoteUser = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Connected to MongoDB');
 
     const user = await User.findOne({ email });
